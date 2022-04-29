4556357798061bcb22fd2d85cec7dbb075998296
kino-ma <ma@kino.ma>
2022-04-30 01:18:03 +0900

    [feat] call github api

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,46 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from '@apollo/client'
+
+declare var GITHUB_TOKEN: string
+console.log({ GITHUB_TOKEN })
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  cache: new InMemoryCache(),
+  headers: {
+    authorization: `Bearer ${GITHUB_TOKEN}`,
+    'user-agent': 'apollo-js',
+  },
+})
+
 export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
+  const { data } = await client.query({
+    query: gql`
+      query ($userName: String!) {
+        user(login: $userName) {
+          contributionsCollection {
+            contributionCalendar {
+              totalContributions
+              weeks {
+                contributionDays {
+                  contributionCount
+                  date
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+    variables: {
+      userName: 'kino-ma',
+    },
+  })
+
+  return new Response(JSON.stringify(data))
 }
